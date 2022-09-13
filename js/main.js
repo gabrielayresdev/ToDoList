6419d726338e78118317c4163843327486154acb
Gabriel Ayres <Gabriel.ar.fort@gmail.com>
2022-09-13 17:50:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* 
+    const tarefa = {
+        "nome": "Trabalho",
+        "descricao": "Trabalho de projetos de carreira",
+        "data": formatData(13/09/2022),
+        "categoria": "Hoje",
+        "favorito:" false,
+    }
+*/
